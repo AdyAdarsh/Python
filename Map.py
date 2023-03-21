d54797a20bb60fa4c2d4326e5b4b45d75fb74617
AdyAdarsh <128449712+AdyAdarsh@users.noreply.github.com>
2023-03-21 12:19:14 +0530

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,7 +1,5 @@
 def double(n):
     return n * 2
-
-
 numbers = [1, 2, 3, 4]
-result = map(double, numbers)  # transforms all the items in list without another iteration
+result = map(double, numbers)  
 print(list(result))
